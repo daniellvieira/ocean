899c29818ccd0ea851635317abe9b6f8c0dcedf9
Daniel Vieira <danielvieiraufc@gmail.com>
2023-08-19 18:21:21 -0300

    feat: add 2fa with qrcode in users

--- Gemfile
+++ Gemfile
@@ -48,10 +48,12 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+# https://github.com/heartcombo/devise
 gem "devise", "~> 4.9"
-
 # https://github.com/tinfoil/devise-two-factor
 gem "devise-two-factor", "~> 5.0"
+# https://github.com/whomwah/rqrcode
+gem "rqrcode", "~> 2.0"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -20,9 +20,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    if should_disable_otp? && !disable_otp!
+      redirect_to edit_registration_path(resource_name)
+      return
+    end
+
+    process_standard_update
+  end
 
   # DELETE /resource
   # def destroy
@@ -38,7 +44,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -46,9 +52,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:otp_attempt])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
@@ -59,4 +65,46 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def should_disable_otp?
+    resource.otp_required_for_login && params[:user][:otp_attempt].present? && params[:user][:current_password].present?
+  end
+
+  def disable_otp!
+    otp_attempt = params[:user][:otp_attempt]
+    current_password = params[:user][:current_password]
+
+    if resource.validate_and_consume_otp!(otp_attempt) && resource.valid_password?(current_password)
+      resource.otp_required_for_login = false
+      resource.save!
+      set_flash_message!(:notice, :otp_disabled)
+      true
+    else
+      set_flash_message!(:alert, appropriate_error_message(otp_attempt, current_password))
+      false
+    end
+  end
+
+  def appropriate_error_message(otp_attempt, current_password)
+    return :invalid_otp unless resource.validate_and_consume_otp!(otp_attempt)
+    return :invalid_password unless resource.valid_password?(current_password)
+
+    :invalid_otp_and_password
+  end
+
+  def process_standard_update
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+
+    if resource_updated
+      set_flash_message_for_update(resource, prev_unconfirmed_email)
+      bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
 end
